1aab008e70f95c17eb28f43b04e669a6a7413d1f
xiaofa <xiaofa2020a@gmail.com>
2020-03-25 17:11:04 +0800

    測試

--- tes.php
+++ tes.php
@@ -1,2 +1,26 @@
 <?php
 require_once 'vendor\autoload.php';
+var_dump(11111111111);
+$a = ['1', '3', '55', '99'];
+$pos = array_search(min($a), $a);
+echo $a[$pos]; //1
+
+$a=[
+    [
+        'a'=>1,
+        'b'=>11111111,
+        'c'=>'啊啊1',
+    ],
+    [
+        'a'=>2,
+        'b'=>22221,
+        'c'=>'啊啊2',
+    ],
+    [
+        'a'=>3,
+        'b'=>33331,
+        'c'=>'啊啊3',
+    ],
+];
+$pos=array_search(max($a),$a);
+var_dump($pos);
